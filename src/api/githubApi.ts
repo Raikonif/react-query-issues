da6bf93557675d4f743ee2b2db55232ce4e7beed
Raikonif <raikonif@gmail.com>
2024-06-05 21:09:49 -0400

    bearer token removed secret

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
 import { VITE_BEARER_TOKEN } from "../constants/api.constants";
 
-export const githubApi = axios.create({ baseURL: "https://api.github.com/repos/facebook/react", headers: { Authorization: `Bearer ${VITE_BEARER_TOKEN}` } })
+export const githubApi = axios.create({
+    baseURL: "https://api.github.com/repos/facebook/react",
+    // headers: { Authorization: `Bearer ${VITE_BEARER_TOKEN}` } 
+})
